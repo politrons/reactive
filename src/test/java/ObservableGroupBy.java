f34ca92aff54d2de080c30e5ceda8d9de526ee3a
ZH11 <Pablo.Garcia@uk.tesco.com>
2015-12-20 22:19:50 +0000

    add switch

--- src/test/java/ObservableGroupBy.java
+++ src/test/java/ObservableGroupBy.java
@@ -7,35 +7,48 @@ import java.util.List;
 
 public class ObservableGroupBy {
 
+    /**
+     * Created a Boolen GroupedObservable
+     */
     @Test
     public void testGroupBy() {
         Observable.just(getPersons())
                   .flatMap(listOfPersons -> Observable.from(listOfPersons)
-                                                      .groupBy(person -> person.sex.equals("male"))).subscribe(booleanPersonGroupedObservable -> {
-            if(booleanPersonGroupedObservable.getKey()){
-                booleanPersonGroupedObservable.asObservable().subscribe(person -> System.out.println("Here the male:" + person.name));
-            }else{
-                booleanPersonGroupedObservable.asObservable().subscribe(person -> System.out.println("Here the female:" + person.name));
-            }
-        });
+                                                      .groupBy(person -> person.sex.equals("male")))
+                  .subscribe(booleanPersonGroupedObservable -> {
+                      if (booleanPersonGroupedObservable.getKey()) {
+                          booleanPersonGroupedObservable.asObservable()
+                                                        .subscribe(person -> System.out.println("Here the male:" + person.name));
+                      } else {
+                          booleanPersonGroupedObservable.asObservable()
+                                                        .subscribe(person -> System.out.println("Here the female:" + person.name));
+                      }
+                  });
     }
 
 
+    /**
+     * Created a GroupedObservable by sex
+     */
     @Test
     public void testGroupBySex() {
         Observable.just(getPersons())
                   .flatMap(listOfPersons -> Observable.from(listOfPersons)
-                                                      .groupBy(person -> person.sex)).subscribe(booleanPersonGroupedObservable -> {
-            switch (booleanPersonGroupedObservable.getKey()){
-                case "male":{
-                    booleanPersonGroupedObservable.asObservable().subscribe(person -> System.out.println("Here the male:" + person.name));
-                    break;
-                }case "female":{
-                    booleanPersonGroupedObservable.asObservable().subscribe(person -> System.out.println("Here the female:" + person.name));
-                    break;
-                }
-            }
-        });
+                                                      .groupBy(person -> person.sex))
+                  .subscribe(booleanPersonGroupedObservable -> {
+                      switch (booleanPersonGroupedObservable.getKey()) {
+                          case "male": {
+                              booleanPersonGroupedObservable.asObservable()
+                                                            .subscribe(person -> System.out.println("Here the male:" + person.name));
+                              break;
+                          }
+                          case "female": {
+                              booleanPersonGroupedObservable.asObservable()
+                                                            .subscribe(person -> System.out.println("Here the female:" + person.name));
+                              break;
+                          }
+                      }
+                  });
     }
 
     private List<Person> getPersons() {
--- src/test/java/ObservableZip.java
+++ src/test/java/ObservableZip.java
@@ -1,14 +1,40 @@
 import org.junit.Test;
 import rx.Observable;
+import rx.Scheduler;
+import rx.schedulers.Schedulers;
 
 
 public class ObservableZip {
 
+
+    private Scheduler scheduler;
+    private Scheduler scheduler1;
+    private Scheduler scheduler2;
+
+    @Test
+    public void testAsyncZip() {
+        scheduler = Schedulers.newThread();
+        scheduler1 = Schedulers.newThread();
+        scheduler2 = Schedulers.newThread();
+        long start =System.currentTimeMillis();
+        Observable.zip(obAsyncString(), obAsyncString1(), obAsyncString2(), (s, s2, s3) -> s.concat(s2)
+                                                                                            .concat(s3))
+                  .observeOn(scheduler)
+                  .subscribe(result->showResult("Async:",start, result));
+    }
+
     @Test
     public void testZip() {
+        long start =System.currentTimeMillis();
         Observable.zip(obString(), obString1(), obString2(), (s, s2, s3) -> s.concat(s2)
-                                                                         .concat(s3))
-                  .subscribe(System.out::println);
+                                                                             .concat(s3))
+                  .subscribe(result->showResult("Sync:",start, result));
+    }
+
+
+    public void showResult(String transactionType, long start, String result){
+        System.out.println(transactionType + String.valueOf(System.currentTimeMillis() - start));
+        System.out.println(result);
     }
 
     public Observable<String> obString() {
@@ -21,5 +47,20 @@ public class ObservableZip {
 
     public Observable<String> obString2() {
         return Observable.just("!");
+    }
+
+    public Observable<String> obAsyncString() {
+        return Observable.just("hello")
+                         .observeOn(scheduler);
+    }
+
+    public Observable<String> obAsyncString1() {
+        return Observable.just(" world")
+                         .observeOn(scheduler1);
+    }
+
+    public Observable<String> obAsyncString2() {
+        return Observable.just("!")
+                         .observeOn(scheduler2);
     }
 }
