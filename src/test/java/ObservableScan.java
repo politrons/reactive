70c46dc91025f08a55cef92d674f14a8c65f7627
ZH11 <Pablo.Garcia@uk.tesco.com>
2015-12-20 18:48:16 +0000

    refactor

--- src/test/java/ObservableScan.java
+++ src/test/java/ObservableScan.java
@@ -12,10 +12,12 @@ public class ObservableScan {
      * apply this function for every item against the previous emitted item from the source
      */
     @Test
-    public void scanObservable(){
-        Integer[] numbers = { 0, 1, 2, 3, 4, 5 };
+    public void scanObservable() {
+        Integer[] numbers = {0, 1, 2, 3, 4, 5};
 
-        Observable.from(numbers).scan((x,y)-> (x + y)).subscribe(System.out::println);
+        Observable.from(numbers)
+                  .scan((lastItemEmitted, newItem) -> (lastItemEmitted + newItem))
+                  .subscribe(System.out::println);
     }
 
 }
