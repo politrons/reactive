d7e3d622ad547ed964e07fc56b16c59e9afae72c
ZH11 <Pablo.Garcia@uk.tesco.com>
2015-12-20 18:03:16 +0000

    refactor

--- src/test/java/ObservableZip.java
+++ src/test/java/ObservableZip.java
@@ -6,7 +6,8 @@ public class ObservableZip {
 
     @Test
     public void testZip() {
-        Observable.zip(obString(), obString1(), String::concat)
+        Observable.zip(obString(), obString1(), obString2(), (s, s2, s3) -> s.concat(s2)
+                                                                         .concat(s3))
                   .subscribe(System.out::println);
     }
 
@@ -16,5 +17,9 @@ public class ObservableZip {
 
     public Observable<String> obString1() {
         return Observable.just(" world");
+    }
+
+    public Observable<String> obString2() {
+        return Observable.just("!");
     }
 }
