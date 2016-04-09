00ac61e50a980ef35278a800cdb3779ad4bee71d
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-04-09 15:12:25 +0100

    add scheduler

---
+++ src/test/java/ObservableScheduler.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+import rx.Observable;
+import rx.Scheduler;
+import rx.schedulers.Schedulers;
+
+
+/**
+ * @author Pablo Perez
+ */
+public class ObservableScheduler {
+
+    private String foo="empty";
+
+    int total = 0;
+
+    private Scheduler scheduler = Schedulers.newThread();
+
+    /**
+     * In this test we prove how when we subscribe a observable using scheduler, this one is executed in another thread, and they dont share attributes.
+     */
+    @Test
+    public void testObservableSubscriptionAsync() {
+        Integer[] numbers = {0, 1, 2, 3, 4};
+
+        Observable.from(numbers)
+                  .flatMap(Observable::just)
+                  .doOnNext(number->{
+                      try {
+                          total+=number;
+                          Thread.sleep(100);
+                      } catch (InterruptedException e) {
+                          e.printStackTrace();
+                      }
+                  })
+                  .subscribeOn(scheduler)
+                  .subscribe(number -> System.out.println("Item emitted:"+total));
+        System.out.println("I finish before the observable finish to items all items:"+total);
+    }
+
+
+}
