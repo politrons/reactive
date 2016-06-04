482ad6a19de2cdd2d7ef21a14daa631910a6329d
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-06-04 15:06:53 +0100

    Refactor of observables

--- src/test/java/rx/observables/ObservableFlatMap.java
+++ src/test/java/rx/observables/ObservableFlatMap.java
@@ -2,12 +2,17 @@ package rx.observables;
 
 import org.junit.Test;
 import rx.Observable;
+import rx.schedulers.Schedulers;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class ObservableFlatMap<T> {
+/**
+ * Sometimes we want to pass an observable through the pipeline, that´s when flatMap come handy.
+ * Also it´ very useful combine flapMap with onSubscribe if we want make our pipeline async.
+ *
+ */
+public class ObservableFlatMap {
 
     static int count = 0;
 
@@ -24,7 +29,7 @@ public class ObservableFlatMap<T> {
         people.add(new Person("Paula", 35, "female"));
         Observable.just(people)
                   .flatMap(Observable::from)
-                  .subscribe(this::showResult, System.out::println, ()-> System.out.println("complete"));
+                  .subscribe(this::showResult, System.out::println, () -> System.out.println("complete"));
     }
 
     private void showResult(Person person) {
@@ -32,21 +37,47 @@ public class ObservableFlatMap<T> {
         System.out.println(++count);
     }
 
-    private String result="";
+    private String result = "";
 
+    /**
+     * Internally in the flatMap, every element in the collection is passed through the pipeline as a new observable.
+     */
     @Test
     public void testFlatMapContact() {
+        long start = System.currentTimeMillis();
         List<String> words = new ArrayList<>();
         words.add("Hello ");
         words.add("Reactive ");
         words.add("World");
         Observable.just(words)
                   .flatMap(Observable::from)
-              .subscribe(this::contactWords, System.out::println, ()-> System.out.println(result));
+                  .subscribe(this::contactWords, System.out::println,
+                             () -> System.out.println("Result " + result + " in " + (System.currentTimeMillis() - start)));
 
     }
 
+    /**
+     * Since we are creating a new observable per word in the array thanks of flapMap, we set that every new observable will be executed in a new thread
+     * Making this pipeline asynchronous
+     */
+    @Test
+    public void testFlatMapAsyncContact() {
+        long start = System.currentTimeMillis();
+        List<String> words = new ArrayList<>();
+        words.add("Hello ");
+        words.add("Reactive ");
+        words.add("World");
+        Observable.from(words)
+                  .flatMap(word -> Observable.just(word)
+                                                .subscribeOn(Schedulers.newThread()))
+                  .subscribe(this::contactWords, System.out::println,
+                             () -> System.out.println("Result " + result + " in " + (System.currentTimeMillis() - start)));
+
+    }
+
+
     private void contactWords(String word) {
+        System.out.println("Thread:"+Thread.currentThread().getName());
         result = result.concat(word);
     }
 
