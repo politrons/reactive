026ff72750954152c9798adabd3827ae31b54cca
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-06-04 19:49:35 +0100

    Refactor of observables

--- src/test/java/rx/observables/ObservableScheduler.java
+++ src/test/java/rx/observables/ObservableScheduler.java
@@ -2,6 +2,7 @@ package rx.observables;
 
 import org.junit.Test;
 import rx.Observable;
+import rx.Subscription;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
@@ -18,23 +19,27 @@ public class ObservableScheduler {
     Integer[] numbers = {0, 1, 2, 3, 4};
 
     /**
-     * In this test we prove how when we subscribe a observable using scheduler, this one is executed in another thread, and they dont share attributes.
+     * In this test we prove how when we subscribe a observable using scheduler, this one is executed in another thread,
+     * and total is in the scope of every thread.
      */
     @Test
-    public void testObservableSubscriptionAsync() {
-        Observable.from(numbers)
+    public void testObservableSubscribeOn() throws InterruptedException {
+        Subscription subscription = Observable.from(numbers)
                   .doOnNext(increaseTotalItemsEmitted())
                   .subscribeOn(Schedulers.newThread())
                   .subscribe(number -> System.out.println("Items emitted:"+total));
-
         System.out.println("I finish before the observable finish.  Items emitted:"+total);
+        while(!subscription.isUnsubscribed()){
+            Thread.sleep(100);
+        }
     }
 
     /**
-     * In this test we prove how when we subscribe a observable using scheduler, this one is executed in another thread, and they dont share attributes.
+     * In this test we prove how when we subscribe a observable ans we not use subscribeOn, this one is executed in the main thread.
+     * And total is in the scope of both
      */
     @Test
-    public void testObservableSubscriptionSync() {
+    public void testObservableSync() {
         Observable.from(numbers)
                   .doOnNext(increaseTotalItemsEmitted())
                   .subscribe(number -> System.out.println("Items emitted:"+total));
@@ -47,7 +52,6 @@ public class ObservableScheduler {
         return number->{
             try {
                 total+=number;
-                System.out.println("current total value:"+total);
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
