6f3cc14a9c8689e2f528eccfb34e2eacbee9f0a6
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-06-05 12:47:14 +0100

    Refactor of observables

--- src/test/java/rx/relay/Relay.java
+++ src/test/java/rx/relay/Relay.java
@@ -1,21 +1,25 @@
 package rx.relay;
 
 import com.jakewharton.rxrelay.BehaviorRelay;
+import com.jakewharton.rxrelay.ReplayRelay;
 import org.junit.Test;
 
 
 /**
  * @author Pablo Perez
+ * Relay is just a type of observable which only implement the onNext function. It means that never invoke onComplete so never unsubscribe the observers.
  */
 public class Relay {
 
 
     /**
-     * Relay is just a subject which subscribe an observer, but it wont unsubscribe once emit the item. So the pipeline keep open
+     * Relay is just an observable which subscribe an observer, but it wont unsubscribe once emit the items. So the pipeline keep open
+     * It should return 1,2,3,4,5 for first observer and just 3, 4, 5 fot the second observer since default relay emit last emitted item,
+     * and all the next items passed to the pipeline.
      */
     @Test
-    public void testRelay() {
-        BehaviorRelay<Object> relay = BehaviorRelay.create("default");
+    public void testRelay() throws InterruptedException {
+        BehaviorRelay<String> relay = BehaviorRelay.create("default");
         relay.subscribe(result -> System.out.println("Observer1:" + result));
         relay.call("1");
         relay.call("2");
@@ -24,4 +28,23 @@ public class Relay {
         relay.call("4");
         relay.call("5");
     }
+
+
+    /**
+     * ReplayRelay it works just like hot observables, once that an observer subscribe, the Relay will replay all items already emitted
+     * to another observer.
+     * it should return 1,2,3,4,5 for both observers.
+     */
+    @Test
+    public void testReplaytRelay() {
+        ReplayRelay<String> relay = ReplayRelay.create();
+        relay.subscribe(result -> System.out.println("Observer1:" + result));
+        relay.call("1");
+        relay.call("2");
+        relay.call("3");
+        relay.subscribe(result -> System.out.println("Observer2:" + result));
+        relay.call("4");
+        relay.call("5");
+    }
+
 }
--- src/test/java/rx/single/SingleFeatures.java
+++ src/test/java/rx/single/SingleFeatures.java
@@ -4,35 +4,46 @@ import org.junit.Test;
 import rx.Single;
 import rx.schedulers.Schedulers;
 
-import static rx.Single.just;
-import static rx.Single.zip;
-
 
 /**
  * @author Pablo Perez
+ * Single is just another variant of observable as relay. In this particular case Single only emit one item.
+ * Since only emit 1 item in the pipeline, single  only accept two Action functions, onSuccess and onError.
  */
 public class SingleFeatures {
 
 
+    /**
+     * Here we can see a basic example where Single just emit the item and onSuccess and onError functions are defined.
+     */
     @Test
     public void testSingle() {
-        just("Single").subscribe(result -> System.out.println("Result: " + result), (error) -> System.out.println("Something went wrong" + error.getMessage()));
+        Single.just("Single").subscribe(result -> System.out.println("Result: " + result),
+                                        (error) -> System.out.println("Something went wrong" + error.getMessage()));
     }
 
 
+    /**
+     * The fact that Single only emit 1 item does not means that cannot use all the ReactiveX features as zip, merge, concat, and so on.
+     * Here we use Zip to run two singles, which every one of those it will emit just 1 item.
+     */
     @Test
     public void testZipSingles() {
-        Single<Integer> single = just(1);
-        Single<Integer> single2 = just(2);
-        zip(single, single2, (s1, s2) -> s1 + s2).subscribe(result -> System.out.println("Result: " + result),
+        Single<Integer> single = Single.just(1);
+        Single<Integer> single2 = Single.just(2);
+        Single.zip(single, single2, (s1, s2) -> s1 + s2)
+              .subscribe(result -> System.out.println("Result: " + result),
                                                             (error) -> System.out.println("Something went wrong" + error.getMessage()));
     }
 
+    /**
+     * Also it´ possible use Single asynchronously using subscribeOn or observerOn
+     */
     @Test
     public void testSinglesAsync() {
         System.out.println("Current thread:" + Thread.currentThread()
                                                      .getName());
-        just("Single").observeOn(Schedulers.newThread())
+        Single.just("Single").subscribeOn(Schedulers.newThread())
                       .subscribe(result -> System.out.println("Async Result in thread: " + Thread.currentThread()
                                                                                                  .getName()),
                                  (error) -> System.out.println("Something went wrong" + error.getMessage()));
