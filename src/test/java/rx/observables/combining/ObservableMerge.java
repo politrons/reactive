aea1ff9a4a029b18691208cd63811a18d72c6790
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-06-06 17:24:01 +0100

    Refactor of observables

--- src/test/java/rx/observables/combining/ObservableMerge.java
+++ src/test/java/rx/observables/combining/ObservableMerge.java
@@ -4,6 +4,10 @@ import org.junit.Test;
 import rx.Observable;
 import rx.observables.transforming.Person;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 
 /**
  * Merge get all observables defined in it, and set into an array,
@@ -22,9 +26,21 @@ public class ObservableMerge {
                   .subscribe(result -> showResult(result.toString()));
     }
 
+    /**
+     * Here we merge two list and we sort both
+     */
+    @Test
+    public void testMergeChains() {
+        Observable.merge(Observable.from(Arrays.asList(1, 2, 13, 11, 5)), Observable.from(Arrays.asList(10, 4, 12, 3, 14, 15)))
+                  .collect(ArrayList<Integer>::new, ArrayList::add)
+                .doOnNext(Collections::sort)
+                .subscribe(System.out::println);
+
+    }
+
+
     private void showResult(String s) {
         System.out.println(s);
-        System.out.println(++count);
     }
 
     public Observable<Person> obPerson() {
