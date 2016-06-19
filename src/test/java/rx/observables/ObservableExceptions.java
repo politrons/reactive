792b535a3bed03fa3cd8e167e280b61f16275581
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-06-19 20:34:20 +0100

    Add filtering package

--- src/test/java/rx/observables/ObservableExceptions.java
+++ src/test/java/rx/observables/ObservableExceptions.java
@@ -2,6 +2,12 @@ package rx.observables;
 
 import org.junit.Test;
 import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
+import rx.observers.TestSubscriber;
+import rx.schedulers.Schedulers;
+
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -9,7 +15,6 @@ import rx.Observable;
  */
 
 /**
- *
  * Observable pipeline does not allow Exception, just runtimeExceptions,
  * that´s why if your code run into pipeline can throw an exception your observable wont compile.
  * In order to fix it you will have to pass through the pipeline runtime exceptions
@@ -26,18 +31,39 @@ public class ObservableExceptions {
 
         Observable.from(numbers)
                   .doOnNext(number -> {
-                          if(number > 3){
-                              try {
-                                  throw new IllegalArgumentException();
-                              } catch (Exception e) {
-                                  throw new RuntimeException(e);
-                              }
+                      if (number > 3) {
+                          try {
+                              throw new IllegalArgumentException();
+                          } catch (Exception e) {
+                              throw new RuntimeException(e);
                           }
+                      }
 
                   })
-                  .doOnError(t-> System.out.println("Expecting illegal argument exception:"+t.getMessage()))
+                  .doOnError(t -> System.out.println("Expecting illegal argument exception:" + t.getMessage()))
                   .subscribe();
 
+    }
+
+    int count = 0;
+
+    /**
+     * Here we can see how onErrorResumeNext works and emit an item in case that an error occur in the pipeline and an exception is propagated
+     */
+    @Test
+    public void observableOnErrorResumeNext() {
+        Subscription subscription = Observable.just(null)
+                                              .map(Object::toString)
+                                              .doOnError(failure -> System.out.println("Error:" + failure.getCause()))
+                                              .retryWhen(errors -> errors.doOnNext(o -> count++)
+                                                                         .flatMap(t -> count > 3 ? Observable.error(t) : Observable.just(null)),
+                                                         Schedulers.newThread())
+                                              .onErrorResumeNext(t -> {
+                                                  System.out.println("Error after all retries:" + t.getCause());
+                                                  return Observable.just("I save the world for extinction!");
+                                              })
+                                              .subscribe(s -> System.out.println(s));
+        new TestSubscriber((Observer) subscription).awaitTerminalEvent(500, TimeUnit.MILLISECONDS);
     }
 
 }
