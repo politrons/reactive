facea93ad0e06a45309d93cdf1295310a793ea01
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-10-31 09:44:54 +0000

    Add stream mutable immutable

--- src/test/java/rx/observables/combining/ObservableMerge.java
+++ src/test/java/rx/observables/combining/ObservableMerge.java
@@ -2,7 +2,9 @@ package rx.observables.combining;
 
 import org.junit.Test;
 import rx.Observable;
+import rx.Scheduler;
 import rx.observables.transforming.Person;
+import rx.schedulers.Schedulers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,28 +20,48 @@ public class ObservableMerge {
     /**
      * Since we merge the two observables, once that we subscribe we will emit both.
      * Shall print
-     *      Person{name='pablo', age=34, sex='null'}
-     *      Person{name='null', age=25, sex='male'}
+     * Person{name='pablo', age=34, sex='null'}
+     * Person{name='null', age=25, sex='male'}
      */
     @Test
     public void testMerge() {
         Observable.merge(obPerson(), obPerson1())
-                  .subscribe(result -> showResult(result.toString()));
+                .subscribe(result -> showResult(result.toString()));
     }
 
     /**
      * Here we merge two list and we sort the list for every new item added into.
      * Shall return
-     *
+     * <p>
      * [1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15]
      */
     @Test
     public void testMergeLists() {
         Observable.merge(Observable.from(Arrays.asList(2, 1, 13, 11, 5)), Observable.from(Arrays.asList(10, 4, 12, 3, 14, 15)))
-                  .collect(ArrayList<Integer>::new, ArrayList::add)
-                  .doOnNext(Collections::sort)
-                  .subscribe(System.out::println);
+                .collect(ArrayList<Integer>::new, ArrayList::add)
+                .doOnNext(Collections::sort)
+                .subscribe(System.out::println);
 
+    }
+
+    /**
+     * Using mergeDelayError we ensure that all items merged are emitted, and if one of them throw an error it
+     * wont finish onComplete callback once all items are emitted but onError
+     */
+    @Test
+    public void testMergeDelayError() {
+        Scheduler scheduler = Schedulers.newThread();
+        Observable.mergeDelayError(
+                Observable.error(new RuntimeException())
+                        .observeOn(scheduler)
+                        .subscribeOn(Schedulers.io()),
+                Observable.just("Hello")
+                        .observeOn(scheduler)
+                        .subscribeOn(Schedulers.io()))
+                .finallyDo(() -> System.out.println("Finally action"))
+                .subscribe(System.out::println,
+                        System.out::println,
+                        () -> System.out.println("On complete it should never happen"));
     }
 
 
@@ -47,11 +69,11 @@ public class ObservableMerge {
         System.out.println(s);
     }
 
-    public Observable<Person> obPerson() {
+    private Observable<Person> obPerson() {
         return Observable.just(new Person("pablo", 34, null));
     }
 
-    public Observable<Person> obPerson1() {
+    private Observable<Person> obPerson1() {
         return Observable.just(new Person(null, 25, "male"));
     }
 
