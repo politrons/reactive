eba6db0b30625983138a6b78dda0dcd774eb38c6
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-12-08 10:59:47 +0000

    Add collect operator

---
+++ src/test/java/rx/observables/utils/ObservableRecursive.java
@@ -0,0 +1,37 @@
+package rx.observables.utils;
+
+import org.junit.Test;
+import rx.Observable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class ObservableRecursive {
+
+    boolean found = false;
+
+    @Test
+    public void runTest() {
+        final Observable<Integer> numbers = Observable.from(getNumbers());
+        recursive(numbers)
+                .collect(ArrayList<Integer>::new, ArrayList::add)
+                .subscribe(System.out::println);
+    }
+
+    public Observable<Integer> recursive(Observable<Integer> numbers) {
+        return numbers.flatMap(number -> {
+            if (number == 4 && !found) {
+                found = true;
+                return Observable.just(number).concatWith(recursive(Observable.from(getNumbers())));
+            }
+            return Observable.just(number);
+        });
+
+    }
+
+    private ArrayList<Integer> getNumbers() {
+        return new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    }
+
+}
