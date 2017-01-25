dc3b469a23e55dd6f3c74173a1a0949c69402d90
ZH11 <Pablo.Garcia@uk.tesco.com>
2017-01-25 09:24:21 +0100

    Add collect operator

--- src/test/java/rx/utils/ObservableMonad.java
+++ src/test/java/rx/utils/ObservableMonad.java
@@ -2,6 +2,7 @@ package rx.utils;
 
 import org.junit.Test;
 import rx.Observable;
+import rx.schedulers.Schedulers;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -15,6 +16,7 @@ public class ObservableMonad {
     private Function<String, String> asyncFunc = new AsyncValue();
     private Function<String, String> syncFunc = new SyncValue();
 
+
     @Test
     public void testMonad() {
         runMonad("Async", asyncFunc);
@@ -23,17 +25,22 @@ public class ObservableMonad {
 
     /**
      * This monad should never be modify, only the function that run in the pipeline.
+     *
      * @param id
      * @param function
      */
     private void runMonad(String id, Function<String, String> function) {
         Observable.just(function)
-                .map(f -> f.apply(id))
-                .filter(val -> !val.isEmpty())
+                .flatMap(f -> Observable.just(f.apply(id))
+                        .doOnNext(val -> System.out.println(Thread.currentThread().getName()))
+                        .subscribeOn(Schedulers.newThread()))//This step it will async
                 .subscribe(System.out::println);
     }
 
 
+    /**
+     * A function which internally get a Promise from an external resource
+     */
     public class AsyncValue implements Function<String, String> {
         @Override
         public String apply(String id) {
