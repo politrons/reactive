7ccd83064c752de3a9bcbc0c0954c2b924fbe7b7
ZH11 <Pablo.Garcia@uk.tesco.com>
2017-02-02 13:55:52 +0100

    Add kryo

---
+++ src/test/java/kryo/KryoUtils.java
@@ -0,0 +1,41 @@
+package kryo;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Created by pabloperezgarcia on 02/02/2017.
+ */
+public class KryoUtils {
+
+    private Kryo kryo = new Kryo();
+
+    @Test
+    public void serializeDeserialize() throws FileNotFoundException {
+        serializeObject();
+        assertTrue(getDeserializeObject() != null);
+    }
+
+    private void serializeObject() throws FileNotFoundException {
+        Output output = new Output(new FileOutputStream("file.bin"));
+        SomeClass someObject = new SomeClass("a","b");
+        kryo.writeObject(output, someObject);
+        output.close();
+    }
+
+    private SomeClass getDeserializeObject() throws FileNotFoundException {
+        Input input = new Input(new FileInputStream("file.bin"));
+        SomeClass someObject = kryo.readObject(input, SomeClass.class);
+        input.close();
+        return someObject;
+    }
+
+}
---
+++ src/test/java/kryo/SomeClass.java
@@ -0,0 +1,17 @@
+package kryo;
+
+/**
+ * Created by pabloperezgarcia on 02/02/2017.
+ */
+class SomeClass {
+
+    private String a;
+    private String b;
+
+    SomeClass(){}
+
+    SomeClass(String a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+}
