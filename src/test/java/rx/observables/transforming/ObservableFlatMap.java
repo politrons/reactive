33e2d216b27acfcf1744c2722bc3da1d35fb426f
ZH11 <Pablo.Garcia@uk.tesco.com>
2017-06-07 09:52:04 +0200

    software craftsmanship

--- src/test/java/rx/observables/transforming/ObservableFlatMap.java
+++ src/test/java/rx/observables/transforming/ObservableFlatMap.java
@@ -135,4 +135,21 @@ public class ObservableFlatMap {
     }
 
 
+    @Test
+    public void thirdDeepLevel() {
+        Observable.from(Arrays.asList("a", "b", "c", "d", "e"))
+                .flatMap(letter -> Observable.from(Arrays.asList(1, 2, 3, 4, 5))
+                        .map(number -> {
+                            System.out.println(letter + ":" + number);
+                            return number;
+                        })
+                        .filter(number -> number == 4)
+                        .flatMap(number -> Observable.from(Arrays.asList("f", "g", "h", "i"))
+                                .map(leter2 -> {
+                                    System.out.println(letter + ":" + number);
+                                    return leter2;
+                                })))
+                .subscribe();
+    }
+
 }
