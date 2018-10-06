ef268871735fa7d9abd4b3476bfd93cfcb12400a
politrons <politrons@gmail.com>
2018-10-06 21:48:13 +0200

    Java 11 features

--- src/test/java/java11/CollectionFeatures.java
+++ src/test/java/java11/CollectionFeatures.java
@@ -10,20 +10,18 @@ import java.util.Set;
 
 public class CollectionFeatures {
 
-
     /**
      * In collection 11 allows the collection's elements to be transferred to a newly created array of the desired runtime type.
      */
     @Test
     public void feature() {
-        final Set<Integer> numbers = Set.of(1, 2, 3, 4);
+        final var numbers = Set.of(1, 2, 3, 4);
         var intArray = numbers.toArray(Integer[]::new);
         System.out.println(Arrays.toString(intArray));
 
-        final Set<String> words = Set.of("hello", "copy", "array", "java");
+        final var words = Set.of("hello", "copy", "array", "java");
         var stringArray = words.toArray(String[]::new);
         System.out.println(Arrays.toString(stringArray));
-
     }
 
 
