d0f35599a43492d020763a543f4e33c2b88badcc
politrons <politrons@gmail.com>
2021-04-23 19:51:57 +0200

    Java 15 features

--- src/test/java/java15/Java15Features.java
+++ src/test/java/java15/Java15Features.java
@@ -8,7 +8,7 @@ public class Java15Features {
      * Java 15 introduce [sealed] class just like Scala had, to create super classes that must be extended
      * by subclass. It also contains a cool feature which is [permits] where as a sealed class, only allow to be
      * extended by some specific classes.
-     *
+     * <p>
      * Here in order to make this whole bound perfect, Javas introduce [non-sealed], which means
      * The subclass must extend a [sealed] class
      */
@@ -17,16 +17,14 @@ public class Java15Features {
     }
 
     public non-sealed class SealedClassExtension extends MySealedClass {
-        public void init(){
+        public void init() {
             System.out.println(helloSealedWorld);
         }
     }
 
     @Test
-    public void sealedFeature(){
+    public void sealedFeature() {
         new SealedClassExtension().init();
     }
-
-
 
 }
