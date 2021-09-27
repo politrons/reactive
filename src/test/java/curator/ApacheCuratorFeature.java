3831aa0db67c8d16050cd09a2a6a7ee43de08bfb
Pablo Pérez García <politrons@gmail.com>
2021-09-27 16:34:26 +0200

    Apache Curator

--- src/test/java/curator/ApacheCuratorFeature.java
+++ src/test/java/curator/ApacheCuratorFeature.java
@@ -20,6 +20,17 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 
+/**
+ * IMPORTANT: In order to make this test works, we need to have a zookeeper running on port [2181]
+ * I personally use docker image: docker pull johnnypark/kafka-zookeeper
+ * And run with: docker run -p 2181:2181 -p 9092:9092 -e ADVERTISED_HOST=127.0.0.1  -e NUM_PARTITIONS=10 johnnypark/kafka-zookeeper
+ *
+ * Apache Curator is a DSL in top of Apache Zookeeper to allow connection management between znodes of the cluster
+ * it also allows distributed lock and counter.
+ * <p>
+ * In this example we connect to Zookeeper two clients, and we check how we can lock the access to one particular logic,
+ * and how we can share a counter between the members of the cluster
+ */
 public class ApacheCuratorFeature {
 
     private final String kafkaServer = "localhost:9092";
@@ -38,16 +49,20 @@ public class ApacheCuratorFeature {
         Thread.sleep(5000);
     }
 
+    /**
+     * For this test we run async each task to emulate a possible Kafka broker.
+     * Once we create the client to communicate wth zooKeeper [CuratorFramework]
+     *
+     * First we create distributed counter [SharedCount] using the client and a specific path, which works just like a fileASystem (/counter/bla)
+     * * Once we have the counter we can use [setCount(x)] to add a value into the counter or [getCount] to obtain the value.
+     *
+     * Then we crete a distributed lock [InterProcessSemaphoreMutex] using the client and a specific path.
+     * * Once we have the sharedLock we can use [acquire] which it will block the next lines of code to be used by
+     * * just one machine, and then we will release the lock with [release]
+     */
     private void lockAndCounter() {
         CompletableFuture.supplyAsync(() -> {
-                    int sleepMsBetweenRetries = 100;
-                    int maxRetries = 3;
-                    RetryPolicy retryPolicy = new RetryNTimes(
-                            maxRetries, sleepMsBetweenRetries);
-
-                    CuratorFramework client = CuratorFrameworkFactory
-                            .newClient("127.0.0.1:2181", retryPolicy);
-                    client.start();
+                    CuratorFramework client = getCuratorFramework();
                     try {
                         SharedCount counter = new SharedCount(client, path, 0);
                         counter.start();
@@ -62,13 +77,32 @@ public class ApacheCuratorFeature {
                         }
                         sharedLock.release();
                     } catch (Exception e) {
-                        e.printStackTrace();
+                        System.out.println(e);
                     }
                     return Void.class;
                 }, Executors.newSingleThreadExecutor()
         );
     }
 
+    /**
+     * Here we create the [CuratorFramework] which behave like the client to communicate with Zookeeper to tell him what to do/
+     * We use Retry policy to at least try 3 times with a backoff of 100ms between each time.
+     */
+    private CuratorFramework getCuratorFramework() {
+        int sleepMsBetweenRetries = 100;
+        int maxRetries = 3;
+        RetryPolicy retryPolicy = new RetryNTimes(
+                maxRetries, sleepMsBetweenRetries);
+
+        CuratorFramework client = CuratorFrameworkFactory
+                .newClient("127.0.0.1:2181", retryPolicy);
+        client.start();
+        return client;
+    }
+
+    /**
+     * Consumer that it should only receive events from one publisher
+     */
     private void kafkaConsumer() {
         Channel<String> consumerChannel = Channel.<String>builder()
                 .name("kafka-connector")
@@ -93,6 +127,9 @@ public class ApacheCuratorFeature {
                 .start();
     }
 
+    /**
+     * Publisher to send events. In this particular test we should invoke this publisher once.
+     */
     private void kafkaPublisher() {
 
         Channel<String> publisherChannel = Channel.<String>builder()
