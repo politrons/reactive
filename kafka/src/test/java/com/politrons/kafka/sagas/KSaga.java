d228b296c022b8ec4535186c5cb6e17bd17e65c7
politrons <politrons@gmail.com>
2022-01-22 21:23:28 +0100

    sagas pattern

--- kafka/src/test/java/com/politrons/kafka/sagas/KSaga.java
+++ kafka/src/test/java/com/politrons/kafka/sagas/KSaga.java
@@ -1,44 +1,47 @@
 package com.politrons.kafka.sagas;
 
 import io.vavr.Function0;
+import io.vavr.control.Try;
+
+import java.util.function.Consumer;
 
 public class KSaga {
 
     public static void main(String[] args) {
         KSaga.withAction(() -> "my local transaction")
-                .withCompensation()
+                .withCompensation(()-> "revert transaction")
                 .withActionChannel()
                 .withCompensationChannel()
                 .build();
     }
 
-
     public static <T> Action withAction(Function0<T> action) {
-        return new Action("");
+        return new Action(action);
     }
 
-    record Action(String value) {
+    record Action<T>(Function0<T> function) {
 
-        public Compensation withCompensation() {
-            return new Compensation();
+        public Compensation withCompensation(Function0<T> compensation) {
+            return new Compensation(this, compensation);
         }
     }
 
-    record Compensation() {
-        public ActionChannel withActionChannel() {
-            return new ActionChannel();
+    record Compensation<T>(Action<T> action,Function0<T> compensation ) {
+        public ActionChannel withActionChannel(Consumer<byte[]> outputMessage) {
+            return new ActionChannel(this, outputMessage);
         }
     }
 
-    record ActionChannel() {
+    record ActionChannel<T>(Compensation<T> tCompensation, Consumer<byte[]> outputMessage) {
+
         public CompensationChannel withCompensationChannel() {
-            return new CompensationChannel();
+            return new CompensationChannel(this);
         }
     }
 
-    record CompensationChannel() {
+    record CompensationChannel<T>(Compensation<T> compensation) {
         public void build() {
-
+            Try.of(compensation.action.function::apply);
         }
     }
 
